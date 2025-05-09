179280f15e81d4c993baac1df65c14a85890286c
Mirko-alcocer <mirkoalc@gmail.com>
2025-05-08 20:59:31 -0500

    Html

---
+++ script.js
@@ -0,0 +1,15 @@
+    // Inicialización de las partículas
+    particlesJS('particles-js', {
+        "particles": {
+            "number": {
+                "value": 80
+            },
+            "size": {
+                "value": 3
+            },
+            "move": {
+                "speed": 1
+            }
+        }
+    });
+});
